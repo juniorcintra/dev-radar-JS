28ca0719ae5587ef2eb8308f7cfb9692d34d46d0
Junior Cintra <junior.developer@outlook.com.br>
2020-01-15 00:08:33 -0300

    first  commit

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+const routes  = Router();
+
+// Métodos HTTP: POST, GET, DELETE, PUT
+
+// Tipos de Parametros: 
+// Query Params: request.query (filtros, ordenação, paginação...)
+// Route Params: request.params (identificar um recurso na alteração ou remoção) 
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index);
+
+module.exports = routes;
