a5379e2ceb897af7bacac89bed5615672f695efe
Junior Cintra <junior.developer@outlook.com.br>
2020-01-19 22:34:27 -0300

    Criação da interface web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@dbomnistack-huxsv.gcp.mongodb.net/week10?retryWrites=true&w=majority',{
@@ -8,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@dbomnistack-huxsv.gcp.mongod
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
