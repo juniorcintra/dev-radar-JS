b8cbc904679513d57ab4ca5adf54f252bfd5a518
Junior Cintra <junior.developer@outlook.com.br>
2020-01-21 21:55:23 -0300

    configuração de websocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
-
+const { setupWebSocket } = require('./websocket');
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@dbomnistack-huxsv.gcp.mongodb.net/week10?retryWrites=true&w=majority',{
     useNewUrlParser: true,
@@ -13,5 +17,4 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@dbomnistack-huxsv.gcp.mongod
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-app.listen(3333);
+server.listen(3333);
